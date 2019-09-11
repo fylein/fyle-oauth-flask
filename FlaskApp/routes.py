2f179db10bce77597dbb7b88a630f2146186b677
Subhajit <subha@Subhajits-MacBook-Air.local>
2019-09-11 14:46:59 +0530

    Updated Code and README

--- FlaskApp/routes.py
+++ FlaskApp/routes.py
@@ -9,7 +9,8 @@ from FlaskApp import app
 from FlaskApp.connectors.fyle import FyleConnector
 
 # Necessary URLs and Client Id and Client Secret
-REDIRECT_URI = os.environ.get("REDIRECT_URL")
+REDIRECT_URI ="http://localhost:8080/refresh_token"
+REFRESH_TOKEN=""
 CLIENT_ID = os.environ.get("CLIENT_ID")
 CLIENT_SECRET = os.environ.get("CLIENT_SECRET")
 BASE_URL = os.environ.get("BASE_URL")
@@ -19,17 +20,28 @@ TOKEN_URL = '{0}/api/oauth/token'.format(BASE_URL)
 
 
 # Index/home page
+#Login page 
 @app.route('/')
-@app.route('/login')
+@app.route('/login',methods=['GET'])
 def login():
     error = None
     return render_template('welcome.html', error=error)
 
 
-# Make authorization
-@app.route('/authorize', methods=['POST'])
+#Make authorization
+@app.route('/login',methods=['POST'])
 def authorize():
     return redirect(AUTHORIZE_URL, code=302)
+
+
+#Logout endpoint 
+@app.route('/logout',methods=['POST'])
+def logout():
+     global REFRESH_TOKEN
+     if request.method == 'POST':
+        REFRESH_TOKEN=""
+     return redirect(url_for('login'))
+
 
 
 '''Displaying Employee profile details 
@@ -40,9 +52,22 @@ def authorize():
 
 @app.route('/profile', methods=['GET'])
 def profile():
+    global REFRESH_TOKEN
+    if REFRESH_TOKEN:
+        fyle_connection = FyleConnector(REFRESH_TOKEN)
+        emp_details = fyle_connection.get_employee_details()
+        # REFRESH_TOKEN=""
+        return render_template('profile.html', emp_data=emp_details)
+    return redirect(url_for('login'))
+
+# To get Refresh Token 
+@app.route('/refresh_token',methods=['GET'])
+def get_refresh_token():
     global CLIENT_SECRET
+    global REFRESH_TOKEN
     global CLIENT_ID
     error = None
+    code = None 
     error = request.args.get('error')
     code = request.args.get('code')
     if code:
@@ -51,8 +76,8 @@ def profile():
                                                        "client_secret": CLIENT_SECRET,
                                                        "code": code})
         data = json.loads(json_response.text)
-        refresh_token = data.get("refresh_token")
-        fyle_connection = FyleConnector(refresh_token)
-        emp_details = fyle_connection.get_employee_details()
-        return render_template('profile.html', emp_data=emp_details)
-    return redirect(url_for('login'))
+        REFRESH_TOKEN = data.get("refresh_token")
+    return redirect(url_for('profile'))
+
+
+    
